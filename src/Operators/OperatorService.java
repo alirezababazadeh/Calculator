3765fa4217513026e862e1333cbb2dfa2f065940
youngbizman <youngbizmann@gmail.com>
2023-07-15 03:28:42 +0330

    add other  operation

--- src/Operators/OperatorService.java
+++ src/Operators/OperatorService.java
@@ -24,31 +24,29 @@ public class OperatorService {
 
     @Operator(name = "%")
     public Double modValues(Double first, Double second) {
-        return 0.0;
+        return first % second;
     }
 
     @Operator(name = "|")
-    public Double orValues(Double first, Double second) {
-        return 0.0;
+    public int orValues(int first, int second) {
+        return first | second ;
     }
 
     @Operator(name = "&")
-    public Double andValues(Double first, Double second) {
-        return 0.0;
+    public int andValues(int first, int second) {
+        return first & second;
     }
 
     @Operator(name = "^")
-    public Double xorValues(Double first, Double second) {
-        return 0.0;
-    }
+    public int xorValues(int first, int second) { return first ^ second;}
 
     @Operator(name = "<<")
-    public Double shiftLeftValues(Double first, Double second) {
-        return 0.0;
+    public int shiftLeftValues(int first, int second) {
+        return first << second ;
     }
 
     @Operator(name = ">>")
-    public Double shiftRightValues(Double first, Double second) {
-        return 0.0;
+    public int shiftRightValues(int first, int second) {
+        return first >> second;
     }
 }
