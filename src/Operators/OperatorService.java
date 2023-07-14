b7bd38222302a690a30d30858de9d4d416005799
Alireza Babazadeh <alireza_bz@yahoo.com>
2023-07-14 13:26:22 +0330

    wip: update OperatorService - add all system functionalities

--- src/Operators/OperatorService.java
+++ src/Operators/OperatorService.java
@@ -1,4 +1,54 @@
 package Operators;
 
 public class OperatorService {
+
+    @Operator(name = "+")
+    public Double plusValues(Double first, Double second) {
+        return 0.0;
+    }
+
+    @Operator(name = "-")
+    public Double minusValues(Double first, Double second) {
+        return 0.0;
+    }
+
+    @Operator(name = "*")
+    public Double multipleValues(Double first, Double second) {
+        return 0.0;
+    }
+
+    @Operator(name = "/")
+    public Double divisionValues(Double first, Double second) {
+        return 0.0;
+    }
+
+    @Operator(name = "%")
+    public Double modValues(Double first, Double second) {
+        return 0.0;
+    }
+
+    @Operator(name = "|")
+    public Double orValues(Double first, Double second) {
+        return 0.0;
+    }
+
+    @Operator(name = "&")
+    public Double andValues(Double first, Double second) {
+        return 0.0;
+    }
+
+    @Operator(name = "^")
+    public Double xorValues(Double first, Double second) {
+        return 0.0;
+    }
+
+    @Operator(name = "<<")
+    public Double shiftLeftValues(Double first, Double second) {
+        return 0.0;
+    }
+
+    @Operator(name = ">>")
+    public Double shiftRightValues(Double first, Double second) {
+        return 0.0;
+    }
 }
